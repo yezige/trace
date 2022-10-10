509403a3c44407c8dc4b489d62506100ff195a7a
yezi <a@liu.app>
2022-10-10 15:33:16 +0800

    auto install zip

--- run.sh
+++ run.sh
@@ -48,32 +48,35 @@ function green() {
 # ---基础函数---
 
 # ---主流程---
-if [ ! -f "~/besttrace4linux/besttrace" ]; then
+workdir=/tmp/besttrace4linux
+if [ ! -f "${workdir}/besttrace" ]; then
+    mkdir -p ${workdir}
+    
     # get besttrace4linux
-    if [ ! -f "~/besttrace4linux.zip" ]; then
-        wget https://cdn.ipip.net/17mon/besttrace4linux.zip
+    if [ ! -f "${workdir}/besttrace4linux.zip" ]; then
+        if [ -z $(command -v wget) ]; then
+            install wget
+        fi
+        wget -P $workdir https://cdn.ipip.net/17mon/besttrace4linux.zip
     fi
 
     # uzip
-    mkdir -p besttrace4linux
     if [ -z $(command -v unzip) ]; then
         install unzip
     fi
-    unzip -o besttrace4linux.zip -d ./besttrace4linux
+    unzip -o ${workdir}/besttrace4linux.zip -d ${workdir}
 
-    chmod +x ./besttrace4linux/besttrace
+    chmod +x ${workdir}/besttrace
 fi
 
-cd ~/besttrace4linux
-
-# clear
+clear
 
 ip_list=(219.141.147.210 202.106.50.1 221.179.155.161 202.96.209.133 210.22.97.1 211.136.112.200 58.60.188.222 210.21.196.6 120.196.165.24 202.112.14.151)
 ip_addr=(北京电信 北京联通 北京移动 上海电信 上海联通 上海移动 深圳电信 深圳联通 深圳移动 成都教育网)
 
 for i in {0..9}; do
     green ${ip_addr[$i]}
-    ./besttrace -q 1 ${ip_list[$i]} | awk 'BEGIN {
+    ${workdir}/besttrace -q 1 ${ip_list[$i]} | awk 'BEGIN {
         FS="  "
     }
     function hl_AS(msg) {
